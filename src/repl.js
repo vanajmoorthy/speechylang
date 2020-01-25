ce100bc980d0dfd1971326700717283e941ab849
vanajmoorthy <vanajmoorthy@gmail.com>
2020-01-26 02:27:39 +0530

    EYOOOOO REPLY DONE THIS BOYO WORKS

--- src/repl.js
+++ src/repl.js
@@ -1,17 +1,34 @@
-const { prompt } = require('inquirer');
-const chalk = require('chalk');
+const { prompt } = require("inquirer");
+const chalk = require("chalk");
 
-const { parseAndEvaluate } = require('./parse-and-evaluate');
+const { parseAndEvaluate } = require("./parse-and-evaluate");
 
-const repl = async () => {};
+const askQuestions = () => {
+	const questions = [
+		{ name: "COMMAND", type: "input", message: chalk.blue(">") }
+	];
+
+	return prompt(questions);
+};
+
+const repl = async () => {
+	try {
+		const answers = await askQuestions();
+		const { COMMAND } = answers;
+
+		if (COMMAND.trim()) {
+			console.log(chalk.yellow(parseAndEvaluate(COMMAND)));
+		}
+	} catch (error) {
+		console.error(error);
+	}
+
+	repl();
+};
 
 if (require.main === module) {
-  console.log(
-    chalk.red(
-      `Welcome to the ${chalk.bgYellow('Dropbear')} Programming Language`,
-    ),
-  );
-  repl();
+	console.log(chalk.red(`Welcome to ${chalk.underline("Speechy")}. Enter your command below.`));
+	repl();
 }
 
 module.exports = repl;
