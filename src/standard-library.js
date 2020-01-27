21d1f6ae9ab593427180252f7cc17e9ab2ac6135
vanajmoorthy <vanajmoorthy@gmail.com>
2020-01-28 02:05:12 +0530

    make min and max fns in std lib more succint

--- src/standard-library.js
+++ src/standard-library.js
@@ -5,22 +5,24 @@ const subtract = all((a, b) => a - b);
 const multiply = all((a, b) => a * b);
 const divide = all((a, b) => a / b);
 const modulo = all((a, b) => a % b);
+const max = (...args) => {
+	return Math.max(...args);
+};
+const min = (...args) => {
+	return Math.min(...args);
+};
 const log = console.log;
 
 const environment = {
-  add,
-  subtract,
-  multiply,
-  divide,
-  modulo,
-  log,
-  pi: Math.PI,
-  max(...args) {
-    return Math.max(...args)
-  },
-  min(...args) {
-    return Math.min(...args)
-  },
+	add,
+	subtract,
+	multiply,
+	divide,
+	modulo,
+	log,
+	pi: Math.PI,
+	max,
+	min
 };
 
 module.exports = { environment };
